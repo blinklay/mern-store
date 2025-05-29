9f911a8b65f06d9cd6bbb5a5941dac010981bf9b
blinklay <danillitvinenkod00@gmail.com>
2025-05-29 21:16:21 +0300

    Install cors package

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ const express = require("express");
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser");
 const router = require("./router");
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
 app.use("/api", router)
 
 const start = async () => {
