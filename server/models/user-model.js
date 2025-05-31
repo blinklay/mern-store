8751d2b8425f8d4fd81160522cae378cb04f1559
blinklay <danillitvinenkod00@gmail.com>
2025-05-31 17:12:48 +0300

    Update User Model

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -5,19 +5,40 @@ const UserSchema = new Schema({
     type: String,
     unique: true,
     required: true,
+    trim: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: 6,
+  },
+  name: {
+    type: String,
+    default: "Пользователь",
+    trim: true,
   },
   cart: [
     {
       product: { type: Schema.Types.ObjectId, ref: "Product" },
       quantity: { type: Number, default: 1 }
     }
-  ]
+  ],
+  avatarUrl: {
+    type: String,
+    default: "",
+  },
+  address: {
+    street: { type: String, default: "" },
+    city: { type: String, default: "" },
+    zip: { type: String, default: "" },
+    country: { type: String, default: "" },
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
 }, {
-
+  timestamps: true
 })
 
 module.exports = model("User", UserSchema)
